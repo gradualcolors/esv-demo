091be6e4b7d00bf239d887028c538b1d9933179d
rai <kat.ucf@gmail.com>
2021-09-11 14:35:13 -0400

    Updated application

---
+++ js/anchorScroll.js
@@ -0,0 +1,14 @@
+﻿function BlazorScrollToId(id) {
+    const element = document.getElementById(id);
+    if (element instanceof HTMLElement) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+        $("#" + id).addClass("highlight");
+        setTimeout(function () {
+            $("#" + id).removeClass("highlight");
+        }, 3000);
+    }
+}
